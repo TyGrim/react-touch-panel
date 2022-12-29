68e7dafc269964c0d817a1de70f16eec09d39db0
Tybo <tysongyorfy@gmail.com>
2022-12-29 14:37:42 -0800

    comments creston signal sending with publishEvents func

--- src/Components/bgChanger/BgChanger.tsx
+++ src/Components/bgChanger/BgChanger.tsx
@@ -2,7 +2,14 @@ import { useState, useRef } from 'react';
 import { Slider } from '@mui/material';
 import Button from 'react-bootstrap/Button';
 import "./bgChanger.css";
-import { publishEvent } from "@crestron/ch5-crcomlib";
+import { publishEvent } from "@crestron/ch5-crcomlib";  
+//join number function takes 3 parameters 
+//format: CrComLib.publishEvent(type, signalName, value) 
+//Types are: 
+//'b' or 'boolean 
+//'n', 'numeric' or 'number': Indicates the value will be a 16-bit integer number between the values of 0 to 65,535 or -32,768 to 32,767 (like an Analog Join)
+//'s' or 'string': Indicates the value will be a string(like a serial join)
+// 'o' or 'object': Indicates the value will be an object. Reserved for future use.
 
 function BgChanger() {
 
@@ -33,7 +40,6 @@ const setRedBackground = () => {
     setBackgroundColor(`rgba(235, 67, 67, ${opacity})`);
     publishEvent('b', "1", true);     //digital join 1 goes high
     publishEvent('b', "1", false);   //digital join 1 goes low
-    console.log('Red button pressed');
 };
 
 const setGreenBackground = () => {
@@ -41,7 +47,6 @@ const setGreenBackground = () => {
     setBackgroundColor(`rgba(132, 202, 19, ${opacity})`);
     publishEvent('b', "2", true);     //digital join 2 goes high
     publishEvent('b', "2", false);   //digital join 2 goes low
-    console.log('Green button pressed');
 };
 
 const setYellowBackground = () => {
@@ -49,7 +54,6 @@ const setYellowBackground = () => {
     setBackgroundColor(`rgba(232, 213, 44, ${opacity})`);
     publishEvent('b', "3", true);     //digital join 3 goes high
     publishEvent('b', "3", false);   //digital join 3 goes low
-    console.log('Yellow button pressed');
 };
 
 return (
